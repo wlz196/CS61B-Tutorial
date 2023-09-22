8306626bcaf7b7ac1e40ada332a25590e34ccff6
Wang Lianzuo <130133159+wlz196@users.noreply.github.com>
2023-09-22 22:35:33 +0800

    Update TestArrayDequeGold.java

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -7,6 +7,7 @@ public class TestArrayDequeGold {
     @Test
     public void Test(){
         int i=0;
+        String errString = "";
         StudentArrayDeque sd = new StudentArrayDeque<>();
         ArrayDequeSolution ad = new ArrayDequeSolution();
         while(true){
@@ -14,26 +15,32 @@ public class TestArrayDequeGold {
             if(numberBetweenZeroAndOne<0.25){
                 sd.addFirst(i);
                 ad.addFirst(i);
+
+                errString += ("addFirst(" + i + ")\n");
                 i++;
             }else if(numberBetweenZeroAndOne<0.5){
                 sd.addLast(i);
                 ad.addLast(i);
+
+                errString += ("addLast(" + i + ")\n");
                 i++;
             }else if(numberBetweenZeroAndOne<0.75){
                 if(ad.isEmpty()|| sd.isEmpty()) continue;
                 Integer tmp1 = (Integer) sd.removeFirst();
                 Integer tmp2 = (Integer) ad.removeFirst();
+                errString += ("removeFirst()\n");
                 if(tmp1 != tmp2) {
-                    assertEquals(tmp2,tmp1);
+                    assertEquals(errString,tmp2,tmp1);
 
                 }
             }else{
                 if(ad.isEmpty()|| sd.isEmpty()) continue;
                 Integer tmp1 = (Integer) sd.removeLast();
                 Integer tmp2 = (Integer) ad.removeLast();
+                errString += ("removeLast()\n");
                 if(tmp1 != tmp2) {
-                    assertEquals(tmp2,tmp1);
-                    
+                    assertEquals(errString,tmp2,tmp1);
+
                 }
             }
         }
