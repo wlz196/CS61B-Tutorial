cadcbc5d79265cead2a41367c0f10af7b7b6e237
Wang Lianzuo <130133159+wlz196@users.noreply.github.com>
2023-09-24 16:35:45 +0800

    1

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -1,10 +1,10 @@
-// TODO: Make sure to make this class a part of the synthesizer package
-// package <package name>;
+package synthesizer;// TODO: Make sure to make this class a part of the synthesizer package
+
 import java.util.Iterator;
 
 //TODO: Make sure to make this class and all of its methods public
 //TODO: Make sure to make this class extend AbstractBoundedQueue<t>
-public class ArrayRingBuffer<T>  {
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T>  {
     /* Index for the next dequeue or peek. */
     private int first;            // index for the next dequeue or peek
     /* Index for the next enqueue. */
@@ -21,6 +21,11 @@ public class ArrayRingBuffer<T>  {
         //       this.capacity should be set appropriately. Note that the local variable
         //       here shadows the field we inherit from AbstractBoundedQueue, so
         //       you'll need to use this.capacity to set the capacity.
+        this.first = 0;
+        this.last = 0;
+        this.capacity = capacity;
+        this.fillCount = 0;
+        rb = (T[]) new Object[capacity];
     }
 
     /**
@@ -29,7 +34,12 @@ public class ArrayRingBuffer<T>  {
      * covered Monday.
      */
     public void enqueue(T x) {
+        if(isFull())
+            throw new RuntimeException("Ring Buffer Overflow");
         // TODO: Enqueue the item. Don't forget to increase fillCount and update last.
+        rb[last] = x;
+        last = (last + 1) % capacity;
+        fillCount ++;
     }
 
     /**
@@ -38,15 +48,47 @@ public class ArrayRingBuffer<T>  {
      * covered Monday.
      */
     public T dequeue() {
-        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update 
+        if(isEmpty())
+            throw new RuntimeException("Ring Buffer Underflow");
+        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update
+        T res = rb[first];
+        first = (first+1) %capacity;
+        fillCount --;
+        return res;
     }
+
+
 
     /**
      * Return oldest item, but don't remove it.
      */
     public T peek() {
+        if(isEmpty()) return null;
         // TODO: Return the first item. None of your instance variables should change.
+        return rb[first];
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new KeyIterator();
     }
 
     // TODO: When you get to part 5, implement the needed code to support iteration.
+    public class KeyIterator implements Iterator {
+        private int ptr;
+
+        public KeyIterator(){
+            ptr = first;
+        }
+        @Override
+        public boolean hasNext() {
+            return ptr!=last;
+        }
+
+        @Override
+        public Object next() {
+            ptr=ptr+1;
+            return rb[ptr];
+        }
+    }
 }
